45d2a84005180ee304217de45d77060a4f0a6aab
Davmoon <brightsun2001@gmail.com>
2023-11-22 21:58:42 +0900

    제비 뽑은 후 뽑은 제비 제거되도록 수정

--- HNUADV_PJ2/jebi.c
+++ HNUADV_PJ2/jebi.c
@@ -10,7 +10,7 @@
 #define DIR_RIGHT	3
 
 void jebi_init(void);
-void fix_zero_curser();
+void fix_zero_cursor();
 void j_move_manual(key_t key);
 bool jebi_placable(int, int);
 void move_jebi(int, int, int);
@@ -30,12 +30,11 @@ void jebi_init(void) {
 	for (int i = 0; i < n_alive; i++) {
 		back_buf[4][i * 2 + 2] = '?';
 	}
-
 	count_r = 0;
-	fix_zero_curser();
+	fix_zero_cursor();
 }
 
-void fix_zero_curser() {
+void fix_zero_cursor() {
 	px[0] = 4; py[0] = 2;
 	back_buf[px[0]][py[0]] = '@';
 }
@@ -63,6 +62,7 @@ void j_move_manual(key_t key) {
 
 // 살아있는 플레이어 수보다 더 나가지 않도록 count_p와 검증
 bool jebi_placable(int row, int col) {
+	// ?가 아닌 경우 못 움직임
 	if (row < 0 || row >= N_ROW ||
 		col < 0 || col >= N_COL ||
 		back_buf[row][col] != '?') {
@@ -86,7 +86,7 @@ void print_rp(int player) {
 
 void ck_kill(int* sel_kill, bool space) {
 	//0번이 살아있는 경우 죽는지 확인
-	if (space && *sel_kill == 0) {
+	/*if (space && *sel_kill == 0) {
 		n_alive--;
 		player[0].is_alive = false;
 		dialog("player 0 fail!");
@@ -96,35 +96,49 @@ void ck_kill(int* sel_kill, bool space) {
 		count_r++;
 		
 		*sel_kill = randint(0, n_alive - 1);
-		return;
-	}
+	}*/
 	int count = 0;
 	for (int i = 0; i < PLAYER_MAX; i++) {
-		if (player[i].is_alive == true) {
-			if (i != 0) {
-				print_rp(i);
-				Sleep(1000);
-			}
-			if (count == *sel_kill) {
-				n_alive--;
-				player[i].is_alive = false;
-				sprintf(msg, "%s %d %s ", msg, i, "dead!");
-				dialog(msg);
-				msg[6] = '\0';//player 외 초기화
+		if (player[i].is_alive == true && i != 0) {
+			print_rp(i);
+			Sleep(1000);
+		}
+		if (player[i].is_alive == true && count == *sel_kill) {
+			n_alive--;
+			player[i].is_alive = false;
+			sprintf(msg, "%s %d %s ", msg, i, "dead!");
+			dialog(msg);
+			msg[6] = '\0';//player 외 초기화
+	
+			//if (i == 0) {
+				//back_buf[px[0]][py[0]] = '?';
+			//}
+			//fix_zero_cursor();
+			//back_buf[4][(n_alive - count) * 2 + 2] = ' ';
 
-				back_buf[px[0]][py[0]] = '?';
-				back_buf[4][n_alive * 2 + 2] = ' ';
-				count_r++;
-				*sel_kill = randint(0, n_alive - 1);
-				fix_zero_curser();
-				return;
-			}
+			count_r++;
+			count++;
+			*sel_kill = randint(0, n_alive - 1);
+			break;
+		}
+		else if(player[i].is_alive == true){
+			back_buf[px[0]][py[0]] = '?';
+			fix_zero_cursor();
+			back_buf[4][(n_alive - count - 1) * 2 + 2] = ' ';
+
 			sprintf(msg, "%s %d %s ", msg, i, "pass!");
 			dialog(msg);
 			msg[6] = '\0';//player 외 초기화
 			count++;
 		}
 	}
+	for (int i = 0; i < PLAYER_MAX; i++) {
+		back_buf[4][i * 2 + 2] = ' ';
+	}
+	for (int i = 0; i < n_alive; i++) {
+		back_buf[4][i * 2 + 2] = '?';
+	}
+	fix_zero_cursor();
 }
 
 void jebi(void) {
@@ -156,7 +170,7 @@ void jebi(void) {
 				j_move_manual(key);
 			}
 		}
-		else {
+		else if (player[0].is_alive == false){
 			ck_kill(&sel_kill, false);
 		}
 
