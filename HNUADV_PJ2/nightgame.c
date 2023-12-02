d94026eb1e992d2d147b533d2a1e594d5966a4ef
Davmoon <brightsun2001@gmail.com>
2023-12-03 00:57:03 +0900

    아이템 교체 작동 확인. 검증중

--- HNUADV_PJ2/nightgame.c
+++ HNUADV_PJ2/nightgame.c
@@ -20,7 +20,7 @@ int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX], itmx[PLAYER_MAX], itmy[P
 
 void ng_init(void) {
 	map_init(15, 40);//#으로 둘러쌓인 sample.c의 실제 플레이 맵 부분
-	int period_set[] = { 300,300,300,300,300,300,300,300,300,300 };
+	int period_set[] = { 500,500,500,500,500,500,500,500,500,500 };
 	//int period_set[] = { 250, 260, 270, 280, 290, 300, 310, 320, 330, 340 };
 
 	int x, y;
@@ -87,82 +87,91 @@ void ngmv_random(int p) {
 }
 
 void itraction(bool check, int itm_or_player_num, int p, int nx, int ny) {
-		
-			//아이템에 접근하는 경우
-			if (check) {
-				if (player[p].hasitem == false) {
-					move_tail(p, nx, ny);
-					itm_take[itm_or_player_num] = false;
-					player[p].hasitem = true;
-					player[p].item = item[itm_or_player_num]; // 포인터 주소값으로 연결해줌.
-					char msgp[30];
-					sprintf_s(msgp, sizeof(msgp), "player %d 아이템 획득", p);
-					dialog(msgp);
-				}
-				else {
-					cg_item(p, itm_or_player_num);
-				}
 
-			}
-			// 아이템 가진 플레이어에 접근하는 경우
-			else {
+	//아이템에 접근하는 경우
+	if (check) {
+		if (player[p].hasitem == false) {
+			move_tail(p, nx, ny);
+			itm_take[itm_or_player_num] = false;
+			player[p].hasitem = true;
+			player[p].item = item[itm_or_player_num]; // 포인터 주소값으로 연결해줌.
+			char msgp[30];
+			sprintf_s(msgp, sizeof(msgp), "player %d 아이템 획득", p);
+			dialog(msgp);
+		}
+		else {
+			cg_item(p, itm_or_player_num);
+		}
 
-			}
+	}
+	// 아이템 가진 플레이어에 접근하는 경우
+	else {
+
+	}
 }
 
 void cg_item(int p, int itmnum) {
-	int choose = randint(0, 1);
-	
+	int choose = randint(0, 1); // 0교환, 1미교환
+	int nnx, nny;
+
+	do {
+		nnx = randint(1, N_ROW - 2);
+		nny = randint(1, N_COL - 2);
+	} while (!placable(nnx, nny));
+
 	if (p == 0) {
 		gotoxy(N_ROW, 0);
 		printf("아이템을 교환하시겠습니까? y/n");
 		while (1) {
 			key_t key = get_key();
 			if (key == K_YES) {
-				int nx, ny;
-				do {
-					nx = randint(1, N_ROW - 2);
-					ny = randint(1, N_COL - 2);
-					
-				} while (!placeable(nx, ny));
-
+				move_tail(p, itmx[itmnum], itmy[itmnum]);
+				itm_take[itmnum] = false;
 				ITEM temp = player[p].item;
 				player[p].item = item[itmnum];
 				item[itmnum] = temp;
+
+				itmx[itmnum] = nnx;
+				itmy[itmnum] = nny;
+				back_buf[nnx][nny] = 'I';
+
 				dialog("0번 아이템 교체됨");
-
-				itmx[itmnum] = nx; itmy[itmnum] = ny;
-				back_buf[nx][ny] = 'I';
-
 				break;
 			}
 			if (key == K_NO) {
-				int nx, ny;
-				do {
-					nx = randint(1, N_ROW - 2);
-					ny = randint(1, N_COL - 2);
-
-				} while (!placeable(nx, ny));
-
-				itmx[itmnum] = nx; itmy[itmnum] = ny;
-				back_buf[nx][ny] = 'I';
+				itmx[itmnum] = nnx;
+				itmy[itmnum] = nny;
+				back_buf[nnx][nny] = 'I';
 
 				gotoxy(N_ROW, 0);
-				char msgp[30];
-				sprintf_s(msgp, sizeof(msgp), "player %d 아이템 무시", p);
-				printf("%s", msgp);
-				
+				printf("교체 거부, 아이템 랜덤 배치");
 				break;
 			}
 		}
 	}
 	else {
-		if (choose == 0) {}
-		else {
+		if (choose == 0) {
+			move_tail(p, itmx[itmnum], itmy[itmnum]);
+			itm_take[itmnum] = false;
+			ITEM temp = player[p].item;
 			player[p].item = item[itmnum];
+			item[itmnum] = temp;
+
+			itmx[itmnum] = nnx;
+			itmy[itmnum] = nny;
+			back_buf[nnx][nny] = 'I';
+
 			char msgp[30];
 			sprintf_s(msgp, sizeof(msgp), "player %d 아이템 교체", p);
 			dialog(msgp);
+		}
+		else {
+			itmx[itmnum] = nnx;
+			itmy[itmnum] = nny;
+			back_buf[nnx][nny] = 'I';
+
+			gotoxy(N_ROW, 0);
+			printf("교체 거부, 아이템 랜덤 배치");
 		}
 	}
 }
@@ -183,7 +192,7 @@ bool ck_near_itm(int p, int* itm_or_player_num) {
 	}
 
 	for (int i = 0; i < n_player; i++) {
-		if (player[i].hasitem && i != p && player[i].is_alive == true ) {
+		if (player[i].hasitem && i != p && player[i].is_alive == true) {
 			// 두 플레이어 좌표중 어느것이 더 클지 모르기 때문에 abs() 절댓값 사용
 			int lena = abs(px[p] - px[i]) + abs(py[p] - py[i]);
 
@@ -195,7 +204,7 @@ bool ck_near_itm(int p, int* itm_or_player_num) {
 	*itm_or_player_num = short_index;
 
 	if (itmT_or_playerF == true) { return true; }
-	else if (itmT_or_playerF == false){ return false; }
+	else if (itmT_or_playerF == false) { return false; }
 }
 
 void nightgame(void) {
@@ -232,7 +241,7 @@ void nightgame(void) {
 		}
 
 
-		
+
 		display();
 		Sleep(10);
 		tick += 10;
