10547f62619b3a4fed9939b7f750ef3df042b404
donghyeonshin <77313884+davidsdh821@users.noreply.github.com>
2023-12-04 02:29:37 +0900

    기능 구현 완료
    가끔 배열이 터지는 문제가 생기는것과 결과에 따른 데이터 넘기기, 다이얼로그 표시 남음

--- HNUADV_PJ2/juldarigi.c
+++ HNUADV_PJ2/juldarigi.c
@@ -16,9 +16,8 @@
 #define DIR_LEFT	2
 #define DIR_RIGHT	3
 
+#define	DATA_FILE	"jjuggumi.dat"
 
-#define PLAYER_MAX		6
-#define ITEM_MAX	10
 
 void team_mate(); //팀설정
 void player_list(); //플레이어 리스트 
@@ -28,24 +27,22 @@ void power(); //힘 계산
 void map(); //맵 생성
 void player_spawn(); //배열안에 플레이어 생성
 void player_change();
-void print_player_list();
-
-
-
+void print_list(); //리스트 출력
 
 int a[2][PLAYER_MAX / 2]; //플레이어 리스트 전역변수로 선언
 char maps[3][29];
 float strs; // 힘
-int z = (29 / 2) - ((PLAYER_MAX / 2) + 1); // 왼쪽으로 잡아당길때의 기준점
-int z2 = (29 / 2) + ((PLAYER_MAX / 2) + 3); //오른쪽으로 잡아당길때의 기준점
-PLAYER dead[PLAYER_MAX]; //죽은 사람의 번호를 넣을 배열
+int z; // 왼쪽으로 잡아당길때의 기준점
+int z2; //오른쪽으로 잡아당길때의 기준점
+PLAYER dead[6]; //죽은 사람의 번호를 넣을 배열
 int dead_count = 0; //죽은 사람이 몇명인지 카운트 하는 배열
 float m_pw = 0;
+bool b_LLiedown = false;
+bool b_RLiedown = false;
+bool b_masteradd = false;
 
 
 void map() { //맵 생성
-
-
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 29; j++) {
 			if (i == 0 || i == 2) {
@@ -72,9 +69,9 @@ void map() { //맵 생성
 }
 
 void player_spawn() { //스폰
-	int x = 0, y = 0, z = (29 / 2 - (PLAYER_MAX - 2)); //x는 1차원배열, y가 2차원 배열의 미지수, z는 줄을 집어넣기위한 카운트
-	for (int i = 0; i < PLAYER_MAX; i++) {
-		if (y >= PLAYER_MAX / 2) {
+	int x = 0, y = 0, z = (29 / 2 - (n_player / 2 + 1)); //x는 1차원배열, y가 2차원 배열의 미지수, z는 줄을 집어넣기위한 카운트
+	for (int i = 0; i < n_player; i++) {
+		if (y >= n_player / 2) {
 			y = 0;
 			x++;
 			for (int j = 0; j < 3; j++) {
@@ -94,45 +91,89 @@ void player_spawn() { //스폰
 }
 
 void player_change() {
-	if (strs < 0) { // 힘이 음수일때
+	if (b_RLiedown || b_LLiedown)
+	{
+		for (int count = 0; count < 2; count++) {
+			if (strs < 0) { // 힘이 음수일때
+				if (maps[1][15] != '-')
+				{
 
-		if (maps[1][15] != '-')
-		{
-			dead[dead_count] = player[atoi(&maps[1][15])];
-			dead_count++;
-			for (int i = 14; i < 14 + ((PLAYER_MAX / 2) + 1); i++) {
+					dead[dead_count] = player[(maps[1][15] - '0')];
+					dead_count++;
+					for (int i = 14; i < 14 + ((n_player / 2) + 1); i++) {
 
-				maps[1][i + 1] = maps[1][i + 2];
+						maps[1][i + 1] = maps[1][i + 2];
+					}
 
+				}
+				else {
+					for (int i = z; i < z + (n_player + 5); i++) {
+						maps[1][i - 1] = maps[1][i];
+					}
+					z--; //기준점 갱신
+				}
 			}
-
-		}
-		else {
-			for (int i = z; i < z + (PLAYER_MAX + 4); i++) {
-				maps[1][i - 1] = maps[1][i];
-
+			else if (strs > 0) { //힘이 양수일때
+				if (maps[1][13] != '-')
+				{
+					dead[dead_count] = player[(maps[1][13] - '0')];
+					dead_count++;
+					for (int i = 14; i > 14 - ((n_player / 2) + 1); i--) {
+						maps[1][i - 1] = maps[1][i - 2];
+					}
+				}
+				else {
+					for (int i = z2; i > z2 - (n_player + 4); i--) {
+						maps[1][i - 1] = maps[1][i - 2];
+					}
+					z2++;//기준점 갱신
+				}
 			}
-			z--; //기준점 갱신
+			else { //힘이 0일때
+				maps[3][29];
+			}
 		}
 	}
-	else if (strs > 0) { //힘이 양수일때
-		if (maps[1][13] != '-')
-		{
-			dead[dead_count] = player[atoi(&maps[1][13])];
-			dead_count++;
-			for (int i = 14; i > 14 - ((PLAYER_MAX / 2) + 1); i--) {
-				maps[1][i - 1] = maps[1][i - 2];
+	else
+	{
+		if (strs < 0) { // 힘이 음수일때
+			if (maps[1][15] != '-')
+			{
+
+				dead[dead_count] = player[(maps[1][15] - '0')];
+				dead_count++;
+				for (int i = 14; i < 14 + ((n_player / 2) + 2); i++) {
+
+					maps[1][i + 1] = maps[1][i + 2];
+				}
+
+			}
+			else {
+				for (int i = z; i < z + (n_player + 6); i++) {
+					maps[1][i - 1] = maps[1][i];
+				}
+				z--; //기준점 갱신
 			}
 		}
-		else {
-			for (int i = z2; i > z2 - (PLAYER_MAX + 4); i--) {
-				maps[1][i - 1] = maps[1][i - 2];
+		else if (strs > 0) { //힘이 양수일때
+			if (maps[1][13] != '-')
+			{
+				dead[dead_count] = player[(maps[1][13] - '0')];
+				dead_count++;
+				for (int i = 14; i > 14 - ((n_player / 2) + 2); i--) {
+					maps[1][i - 1] = maps[1][i - 2];
+				}
 			}
-			z2++;//기준점 갱신
+			else {
+				for (int i = z2; i > z2 - (n_player + 5); i--) {
+					maps[1][i - 1] = maps[1][i - 2];
+				}
+				z2++;//기준점 갱신
+			}
 		}
-	}
-	else { //힘이 0일때
-		maps[3][29];
+		else { //힘이 0일때
+			maps[3][29];
+		}
 	}
 }
 
@@ -147,14 +188,34 @@ void gamemaster() {
 		ch = _getch();  // 키 입력을 대기하고 입력된 키를 가져옴
 
 		if (ch == 'z' || ch == 'Z') {
+			b_masteradd = true;
 			m_pw--;
 		}
 		else if (ch == '/') {
+			b_masteradd = true;
 			m_pw++;
 		}
 		else if (ch == 'q' || ch == 'Q') {
 			printf("\nQuitting the program.\n");
 
+		}
+		else if (ch == 'x' || ch == 'X') {
+			printf("\n왼쪽 팀 눕기 사용.\n");
+			b_LLiedown = true;
+			for (int i = 0; i < n_player / 2; i++) {
+				if (player[a[0][i]].stamina > 0) {
+					player[a[0][i]].stamina = (int)player[a[0][i]].stamina * 0.7;
+				}
+			}
+		}
+		else if (ch == '.') {
+			printf("\n오른쪽 팀 눕기 사용.\n");
+			b_RLiedown = true;
+			for (int i = 0; i < n_player / 2; i++) {
+				if (player[a[1][i]].stamina > 0) {
+					player[a[1][i]].stamina = (int)player[a[1][i]].stamina * 0.7;
+				}
+			}
 		}
 	}
 
@@ -204,36 +265,82 @@ void game() { // 게임 조작, 플레이
 				printf("\n");
 			}
 
-
+			player_list(); //리스트 생성 
 
 			power();
-			player_list();
+
 			player_change();
+
+			b_LLiedown = false;
+			b_RLiedown = false;
 
 			// 다시 시작 시간을 현재 시간으로 업데이트
 			start_time = clock();
 		}
 		gamemaster();
+
 	}
 
 }
 
 void power() { // 힘 계산
-	int powers[2];
+	float powers[2] = { 0.0, 0.0 };
 
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < PLAYER_MAX / 2; j++) {
-			if (player[a[i][j]].hasitem) {
-				powers[i] += player[a[i][j]].str + player[a[i][j]].item.str_buf;
+	for (int i = 0; i < n_player / 2; i++) {
+		if (!(player[a[0][i]].status == 1 || player[a[0][i]].status == 3))
+		{
+			if (b_LLiedown) {
+				if (player[a[0][i]].hasitem) {
+					powers[0] += (player[a[0][i]].str + player[a[0][i]].item.str_buf) * 2.0;
+				}
+				else
+				{
+					powers[0] += player[a[0][i]].str * 2.0;
+				}
 			}
-			else
-			{
-				powers[i] += player[a[i][j]].str;
+			else {
+				if (player[a[0][i]].hasitem) {
+					powers[0] += player[a[0][i]].str + player[a[0][i]].item.str_buf;
+				}
+				else
+				{
+					powers[0] += player[a[0][i]].str;
+				}
 			}
 		}
 	}
-	strs = (powers[0] - powers[1]) + m_pw;
 
+	for (int i = 0; i < n_player / 2; i++) {
+		if (!(player[a[1][i]].status == 1 || player[a[1][i]].status == 3))
+		{
+			if (b_RLiedown) {
+				if (player[a[1][i]].hasitem) {
+					powers[1] += (player[a[1][i]].str + player[a[1][i]].item.str_buf) * 2.0;
+				}
+				else
+				{
+					powers[1] += player[a[1][i]].str * 2.0;
+				}
+			}
+			else {
+				if (player[a[1][i]].hasitem) {
+					powers[1] += player[a[1][i]].str + player[a[1][i]].item.str_buf;
+				}
+				else
+				{
+					powers[1] += player[a[1][i]].str;
+				}
+			}
+		}
+	}
+
+	if (b_masteradd) {
+		strs = (powers[1] - powers[0]) + m_pw;
+		m_pw = 0;
+	}
+	else {
+		strs = powers[1] - powers[0];
+	}
 	printf("str: %.1f\n", strs);
 }
 
@@ -242,39 +349,59 @@ void power() { // 힘 계산
 void player_list() {
 
 
-
 	printf("no. of player left : %d\n", sizeof(player) / sizeof(PLAYER));
-	for (int i = 0; i < PLAYER_MAX; i++) {
-		printf("player %d: ", i);
-		if (player[i].is_alive == true) {
-			char a = dead[i].name;
-			char b = player[i].name;
-			if (strcmp(dead[i].name, player[i].name) == 0) {
-				printf("alive\n"); //이미 살았던 플레이어는 죽지 않음 대신 **개로 표시
-				player[i].hasitem = false; //플레이어의 아이템 몰수
-				player[i].intel = player[i].intel / 2; // 플레이어의 지능 반토막
+	for (int i = 0; i < n_player; i++) { //모든 플레이어가 과거 죽었는지 살았는지 확인하는 반복문
+		if (player[i].is_alive == true) { //과거 살았던 상태인지
+			player[i].status = 2;
+			for (int j = 0; j < dead_count; j++) { //리스트 내의 플레이어와 죽은 플레이어를 대조함 
+				//플레이어가 죽을 수록 카운트는 늘어남
+				if (!strcmp(dead[j].name, player[i].name)) {
+					//현재 반복문 안에 있는 플레이어와 실시간으로 갱신되는 죽은 플레이어와 대조
+					//만약 현재 플레이어와 죽은 플레이어의 이름이 같다면 
+					//사망처리를 해야하지만, 현재 플레이어는 과거 게임에서 생존했기 때문에 
+					player[i].status = 1; //탈락한적 없는 플레이어는 이기에 **표시 마크
+				}
 			}
-			else {
-				printf("alive\n");
-			}
-
 		}
-		else {
-			player[i].hasitem = false; //탈락한 플레이어의 아이템 제거
-			char a = dead[i].name;
-			char b = player[i].name;
-			if (strcmp(dead[i].name, player[i].name) == 0) {
-				printf("DEAD\n");
+		else { //죽었던 상태인지 확인
+			player[i].status = 4;
+			//죽었던 상태에서 위의 반복문을 한번더 체크
+			for (int j = 0; j < dead_count; j++) {
+				if (!strcmp(dead[j].name, player[i].name)) { // 죽은 이력이 있으면서 탈락했다면
+					player[i].status = 3; //진짜 탈락
+				}
 			}
-			else {
-				printf("alive*\n"); //죽었던 플레이어는 *로 표시
-				player[i].is_alive == true; //죽었던 플레이어 부활
-			}
-
-
 		}
-
 	}
+	print_list(); //리스트 출력
+
+
+
+}
+
+void print_list() {
+	for (int i = 0; i < n_player; i++) {
+
+		switch (player[i].status) {
+		case 1:
+			printf("player %d: alive**\n", i); //탈락하지 않은 플레이어 죽으면 **
+			break;
+		case 2:
+			printf("player %d: alive\n", i); //살아있는 상태
+			break;
+		case 3:
+			printf("player %d: DEAD\n", i); //플레이어 사망
+			break;
+		case 4:
+			printf("player %d: alive*\n", i); //탈락했던 사람
+			break;
+
+		default:
+			break;
+
+		}
+	}
+
 }
 
 //팀 설정
@@ -282,7 +409,7 @@ void team_mate() {
 	int temp;
 	int x = 0, y = 0;
 
-	for (int j = 0; j < PLAYER_MAX; j++) {
+	for (int j = 0; j < n_player; j++) {
 		if (j == 0 || j % 2 == 0) {
 			a[0][0 + x] = j;
 			x++;
@@ -293,8 +420,8 @@ void team_mate() {
 		}
 	} //유저 데이터 넣기
 
-	for (int i = 0; i < PLAYER_MAX / 2; i++) { //왼쪽 팀 정렬
-		for (int j = 0; j < ((PLAYER_MAX / 2) - 1) - i; j++) {
+	for (int i = 0; i < n_player / 2; i++) { //왼쪽 팀 정렬
+		for (int j = 0; j < ((n_player / 2) - 1) - i; j++) {
 			if (a[0][j] < a[0][j + 1]) {	// 버블 정렬 사용
 				temp = a[0][j];
 				a[0][j] = a[0][j + 1];
@@ -308,14 +435,46 @@ void team_mate() {
 
 }
 
+int juldarigi_init(void) {
+	srand((unsigned int)time(NULL));
 
+	FILE* fp;
+	fopen_s(&fp, DATA_FILE, "r");
+	if (fp == NULL) {
+		return -1;
+	}
+
+	fscanf_s(fp, "%d", &n_player);
+
+	n_alive = n_player; //살아있는 플레이어 = 처음 플레이어 수
+
+	for (int i = 0; i < n_player; i++) {
+		// 아직 안 배운 문법(구조체 포인터, 간접참조연산자)
+		PLAYER* p = &player[i];
+
+		fscanf_s(fp, "%s%d%d", p->name, (unsigned int)sizeof(p->name), &(p->intel), &(p->str));
+	}
+
+	fscanf_s(fp, "%d", &n_item);
+	for (int i = 0; i < n_item; i++) {
+		fscanf_s(fp, "%s%d%d%d", item[i].name, (unsigned int)sizeof(item[i].name), &(item[i].intel_buf), &(item[i].str_buf), &(item[i].stamina_buf));
+	}
+
+	fclose(fp);
+
+	z = (29 / 2) - ((n_player / 2) + 1);
+	z2 = (29 / 2) + ((n_player / 2) + 3);
+	return 0;
+}
 
 
 void juldarigi() {
-	player[0].hasitem = true;
-	player[0].item.str_buf = 2;
-	player_list();
+	player[0].is_alive = false;
+
+	juldarigi_init();
+
 	team_mate();
+
 
 	map();
 
